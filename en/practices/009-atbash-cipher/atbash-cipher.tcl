7410a849cffd4dc157b6a1363f35329e8edfba2d
quannguyen247 <nguyendongquan247@gmail.com>
2026-06-03 15:24:20 +0700

    Feat(practices): Complete solution for 009-atbash-cipher (vi & en)

--- en/practices/009-atbash-cipher/atbash-cipher.tcl
+++ en/practices/009-atbash-cipher/atbash-cipher.tcl
@@ -33,16 +33,37 @@
 # YOUR SOLUTION CODE BELOW
 # ==============================================================================
 
-namespace eval atbash {
-    namespace export encode decode
-    namespace ensemble create
-
-    proc encode {} {
-        throw {NOT_IMPLEMENTED} "Implement this procedure."
+proc encode {phrase} {
+    set clean [regsub -all {[^a-zA-Z0-9]} $phrase ""]
+    set clean [string tolower $clean]
+    set cipher ""
+    set count 0
+    foreach char [split $clean ""] {
+        if {[string is alpha $char]} {
+            set c [format %c [expr {219 - [scan $char %c]}]]
+        } else {
+            set c $char
+        }
+        if {$count > 0 && $count % 5 == 0} {
+            append cipher " "
+        }
+        append cipher $c
+        incr count
     }
-
-    proc decode {} {
-        throw {NOT_IMPLEMENTED} "Implement this procedure."
-    }
+    return $cipher
 }
 
+proc decode {phrase} {
+    set clean [regsub -all {[^a-zA-Z0-9]} $phrase ""]
+    set clean [string tolower $clean]
+    set plain ""
+    foreach char [split $clean ""] {
+        if {[string is alpha $char]} {
+            set c [format %c [expr {219 - [scan $char %c]}]]
+        } else {
+            set c $char
+        }
+        append plain $c
+    }
+    return $plain
+}
