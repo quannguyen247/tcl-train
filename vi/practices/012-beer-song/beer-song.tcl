bfe2d02d770ed3760116c465302d7cf2b8207029
quannguyen247 <nguyendongquan247@gmail.com>
2026-06-05 08:35:27 +0700

    Feat(practices): Complete solution for 012-beer-song (vi & en)

--- vi/practices/012-beer-song/beer-song.tcl
+++ vi/practices/012-beer-song/beer-song.tcl
@@ -313,7 +313,27 @@
 # YOUR SOLUTION CODE BELOW
 # ==============================================================================
 
-proc beerSong {start take} {
-    throw {NOT_IMPLEMENTED} "Implement this procedure."
+proc verse {n} {
+    if {$n == 0} {
+        return "No more bottles of beer on the wall, no more bottles of beer.
+Go to the store and buy some more, 99 bottles of beer on the wall."
+    } elseif {$n == 1} {
+        return "1 bottle of beer on the wall, 1 bottle of beer.
+Take it down and pass it around, no more bottles of beer on the wall."
+    } elseif {$n == 2} {
+        return "2 bottles of beer on the wall, 2 bottles of beer.
+Take one down and pass it around, 1 bottle of beer on the wall."
+    } else {
+        return "$n bottles of beer on the wall, $n bottles of beer.
+Take one down and pass it around, [expr {$n - 1}] bottles of beer on the wall."
+    }
 }
 
+proc beerSong {start {take 1}} {
+    set verses {}
+    for {set i $start} {$i > $start - $take} {incr i -1} {
+        lappend verses [verse $i]
+    }
+    return [join $verses "
+"]
+}
