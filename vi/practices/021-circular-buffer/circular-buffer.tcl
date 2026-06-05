caa4c52f36c6d079f0a99d8de1ba6de8eb6d9c6b
quannguyen247 <nguyendongquan247@gmail.com>
2026-06-05 14:04:17 +0700

    Feat(practices): Complete solution for 021-circular-buffer (vi & en)

--- vi/practices/021-circular-buffer/circular-buffer.tcl
+++ vi/practices/021-circular-buffer/circular-buffer.tcl
@@ -65,26 +65,46 @@
 # ==============================================================================
 
 oo::class create CircularBuffer {
-    constructor {cap} {
-        throw {NOT_IMPLEMENTED} "Implement this class."
+    variable cap
+    variable buffer
+
+    constructor {capacity} {
+        set cap $capacity
+        set buffer {}
     }
 
     method empty? {} {
+        return [expr {[llength $buffer] == 0}]
     }
 
     method full? {} {
+        return [expr {[llength $buffer] == $cap}]
     }
 
     method read {} {
+        if {[my empty?]} {
+            error "buffer is empty"
+        }
+        set item [lindex $buffer 0]
+        set buffer [lrange $buffer 1 end]
+        return $item
     }
 
-    method write {value} {
+    method write {item} {
+        if {[my full?]} {
+            error "buffer is full"
+        }
+        lappend buffer $item
     }
 
-    method overwrite {value} {
+    method overwrite {item} {
+        if {[my full?]} {
+            set buffer [lrange $buffer 1 end]
+        }
+        lappend buffer $item
     }
 
     method clear {} {
+        set buffer {}
     }
 }
-
