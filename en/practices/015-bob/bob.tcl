3fab08744cd8e976f5840ff689dd38c02ebd6eb1
quannguyen247 <nguyendongquan247@gmail.com>
2026-06-05 16:07:36 +0700

    Feat(practices): Complete solution for 015-bob (vi & en)

--- en/practices/015-bob/bob.tcl
+++ en/practices/015-bob/bob.tcl
@@ -25,7 +25,21 @@
 # YOUR SOLUTION CODE BELOW
 # ==============================================================================
 
-proc heyBob {input} {
-    throw {NOT_IMPLEMENTED} "Implement this procedure."
+proc heyBob {stimulus} {
+    set s [string trim $stimulus]
+    if {$s eq ""} {
+        return "Fine. Be that way!"
+    }
+    set is_yelling [expr {[regexp {[A-Z]} $s] && ![regexp {[a-z]} $s]}]
+    set is_question [expr {[string match "*\?" $s]}]
+    if {$is_yelling && $is_question} {
+        return "Calm down, I know what I'm doing!"
+    }
+    if {$is_yelling} {
+        return "Whoa, chill out!"
+    }
+    if {$is_question} {
+        return "Sure."
+    }
+    return "Whatever."
 }
-
