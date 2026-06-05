efc62ab557ab90a833d48990646b31b9f465eb4e
quannguyen247 <nguyendongquan247@gmail.com>
2026-06-05 14:58:07 +0700

    Feat(practices): Complete solution for 013-binary-search (vi & en)

--- vi/practices/013-binary-search/binary-search.tcl
+++ vi/practices/013-binary-search/binary-search.tcl
@@ -35,7 +35,19 @@
 # YOUR SOLUTION CODE BELOW
 # ==============================================================================
 
-proc binarySearch {haystack needle} {
-    throw {NOT_IMPLEMENTED} "Implement this procedure."
+proc binarySearch {list target} {
+    set low 0
+    set high [expr {[llength $list] - 1}]
+    while {$low <= $high} {
+        set mid [expr {($low + $high) / 2}]
+        set val [lindex $list $mid]
+        if {$val == $target} {
+            return $mid
+        } elseif {$val < $target} {
+            set low [expr {$mid + 1}]
+        } else {
+            set high [expr {$mid - 1}]
+        }
+    }
+    error "value not in list"
 }
-
