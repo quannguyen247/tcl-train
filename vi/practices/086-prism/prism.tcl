8a6f13ef33e16ddf53af64e148125d0f7dadab27
quannguyen247 <nguyendongquan247@gmail.com>
2026-06-17 11:38:19 +0700

    Feat(practices): Complete solution for 086-prism

--- vi/practices/086-prism/prism.tcl
+++ vi/practices/086-prism/prism.tcl
@@ -46,7 +46,55 @@
 
 #!/usr/bin/env tclsh
 
+proc nextPrism {x y angle prisms} {
+    set radians [expr {$angle * acos(-1) / 180.0}]
+    set dx [expr {cos($radians)}]
+    set dy [expr {sin($radians)}]
+    set nearest {}
+    set bestDistance Inf
+
+    # Tìm prism gần nhất nằm trên tia laser
+    foreach prism $prisms {
+        set vx [expr {[dict get $prism x] - $x}]
+        set vy [expr {[dict get $prism y] - $y}]
+        set distance [expr {$vx * $dx + $vy * $dy}]
+        set offset [expr {abs($vx * $dy - $vy * $dx)}]
+
+        # Prism phải nằm phía trước và gần đường tia
+        if {$distance > 1e-9 && $offset < 1e-2
+            && $distance < $bestDistance} {
+            set nearest $prism
+            set bestDistance $distance
+        }
+    }
+    return $nearest
+}
+
 proc findSequence {input} {
-    throw {NOT_IMPLEMENTED} "Implement this procedure."
+    set start [dict get $input start]
+    set prisms [dict get $input prisms]
+    set x [dict get $start x]
+    set y [dict get $start y]
+    set angle [dict get $start angle]
+    set sequence {}
+
+    while {true} {
+        set prism [nextPrism $x $y $angle $prisms]
+        if {$prism eq {}} {
+            break
+        }
+
+        # Ghi nhận prism vừa bị laser chiếu
+        lappend sequence [dict get $prism id]
+        set x [dict get $prism x]
+        set y [dict get $prism y]
+
+        # Góc mới bằng góc hiện tại cộng góc khúc xạ
+        set angle [expr {
+            fmod($angle + [dict get $prism angle], 360.0)
+        }]
+    }
+
+    return $sequence
 }
 
