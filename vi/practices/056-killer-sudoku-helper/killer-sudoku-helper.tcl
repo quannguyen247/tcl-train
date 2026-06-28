ab47af490bf8ff58ea31d9a7313161aa8cdab46f
quannguyen247 <nguyendongquan247@gmail.com>
2026-06-28 14:24:13 +0700

    Feat(practices): Complete solution for 056-killer-sudoku-helper

--- vi/practices/056-killer-sudoku-helper/killer-sudoku-helper.tcl
+++ vi/practices/056-killer-sudoku-helper/killer-sudoku-helper.tcl
@@ -91,7 +91,24 @@
 # YOUR SOLUTION CODE BELOW
 # ==============================================================================
 
+proc cageSearch {sum size exclude start chosen} {
+    if {$size == 0} {
+        return [expr {$sum == 0 ? [list $chosen] : {}}]
+    }
+
+    set result {}
+    for {set digit $start} {$digit <= 9} {incr digit} {
+        if {$digit in $exclude || $digit > $sum} {
+            continue
+        }
+        lappend result {*}[cageSearch [expr {$sum - $digit}] \
+            [expr {$size - 1}] $exclude [expr {$digit + 1}] \
+            [concat $chosen $digit]]
+    }
+    return $result
+}
+
 proc combinations {sum size exclude} {
-    return {}
+    return [cageSearch $sum $size $exclude 1 {}]
 }
 
