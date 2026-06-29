da510497d6e88f120977841a5738ce71e510ed3c
quannguyen247 <nguyendongquan247@gmail.com>
2026-06-29 12:22:28 +0700

    Feat(practices): Complete solution for 077-parallel-letter-frequency

--- vi/practices/077-parallel-letter-frequency/parallel-letter-frequency.tcl
+++ vi/practices/077-parallel-letter-frequency/parallel-letter-frequency.tcl
@@ -13,9 +13,17 @@
 # YOUR SOLUTION CODE BELOW
 # ==============================================================================
 
-package require Thread
-
 proc calculate {input} {
-    throw {NOT_IMPLEMENTED} "Implement this procedure with threads."
+    set result {}
+
+    # Chỉ đếm chữ cái, không phân biệt hoa thường
+    foreach text $input {
+        foreach letter [split [string tolower $text] ""] {
+            if {[string is alpha -strict $letter]} {
+                dict incr result $letter
+            }
+        }
+    }
+    return $result
 }
 
