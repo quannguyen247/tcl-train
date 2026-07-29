d1b867c72022ec2ceb58f4a3952a74cf5ca4dceb
quannguyen247 <nguyendongquan247@gmail.com>
2026-07-29 11:08:00 +0700

    refactor(practices): improve affine-cipher solution

--- practices/003-affine-cipher/affine-cipher.tcl
+++ practices/003-affine-cipher/affine-cipher.tcl
@@ -1,65 +1,58 @@
-proc encode {key phrase} {
-    lassign $key a b
-    if {[coprime $a 26] != 1} {
-        error "a and m must be coprime."
-    }
-    set text [regsub -all {[^a-zA-Z0-9]} $phrase ""]
-    set text [string tolower $text]
-    set cipher ""
-    set count 0
-    foreach char [split $text ""] {
-        if {[string is digit $char]} {
-            set c $char
-        } else {
-            set x [expr {[scan $char %c] - 97}]
-            set y [expr {($a * $x + $b) % 26}]
-            set c [format %c [expr {$y + 97}]]
-        }
-        if {$count > 0 && $count % 5 == 0} {
-            append cipher " "
-        }
-        append cipher $c
-        incr count
-    }
-    return $cipher
-}
-
-proc decode {key phrase} {
-    lassign $key a b
-    if {[coprime $a 26] != 1} {
-        error "a and m must be coprime."
-    }
-    set m_inv [mod_inverse $a 26]
-    set text [regsub -all {[^a-zA-Z0-9]} $phrase ""]
-    set text [string tolower $text]
-    set plain ""
-    foreach char [split $text ""] {
-        if {[string is digit $char]} {
-            append plain $char
-        } else {
-            set y [expr {[scan $char %c] - 97}]
-            set x [expr {($m_inv * ($y - $b)) % 26}]
-            if {$x < 0} { set x [expr {$x + 26}] }
-            append plain [format %c [expr {$x + 97}]]
-        }
-    }
-    return $plain
-}
-
-proc coprime {a b} {
+proc gcd {a b} {
     while {$b != 0} {
-        set t $b
-        set b [expr {$a % $b}]
-        set a $t
+        set t [expr {$a % $b}]
+        set a $b
+        set b $t
     }
     return $a
 }
 
-proc mod_inverse {a m} {
-    for {set x 1} {$x < $m} {incr x} {
-        if {[expr {($a * $x) % $m}] == 1} {
+proc get_mmi {a} {
+    if {[gcd $a 26] != 1} {
+        error "a and m must be coprime."
+    }
+    for {set x 1} {$x < 26} {incr x} {
+        if {($a * $x) % 26 == 1} {
             return $x
         }
     }
-    return 1
+    error "a and m must be coprime."
+}
+
+proc affine_encode {phrase a b} {
+    get_mmi $a
+
+    set result ""
+    set phrase [string tolower $phrase]
+
+    foreach char [split $phrase ""] {
+        if {[regexp {[a-z]} $char]} {
+            set x [expr {[scan $char %c] - 97}]
+            set y [expr {($a * $x + $b) % 26}]
+            append result [format %c [expr {$y + 97}]]
+        } elseif {[regexp {[0-9]} $char]} {
+            append result $char
+        }
+    }
+
+    return [join [regexp -all -inline {.{1,5}} $result] " "]
+}
+
+proc affine_decode {phrase a b} {
+    set a_inv [get_mmi $a]
+
+    set result ""
+    set phrase [string map {" " ""} [string tolower $phrase]]
+
+    foreach char [split $phrase ""] {
+        if {[regexp {[a-z]} $char]} {
+            set y [expr {[scan $char %c] - 97}]
+            set x [expr {(($a_inv * ($y - $b)) % 26 + 26) % 26}]
+            append result [format %c [expr {$x + 97}]]
+        } elseif {[regexp {[0-9]} $char]} {
+            append result $char
+        }
+    }
+
+    return $result
 }
