fb5fa6c0db4a60c3065b8dd51655b63bb4f0f85d
quannguyen247 <nguyendongquan247@gmail.com>
2026-07-29 17:10:04 +0700

    refactor(practices): improve bob solution

--- practices/015-bob/bob.tcl
+++ practices/015-bob/bob.tcl
@@ -1,18 +1,11 @@
 proc heyBob {stimulus} {
     set s [string trim $stimulus]
-    if {$s eq ""} {
-        return "Fine. Be that way!"
-    }
-    set is_yelling [expr {[regexp {[A-Z]} $s] && ![regexp {[a-z]} $s]}]
-    set is_question [expr {[string match "*\?" $s]}]
-    if {$is_yelling && $is_question} {
-        return "Calm down, I know what I'm doing!"
-    }
-    if {$is_yelling} {
-        return "Whoa, chill out!"
-    }
-    if {$is_question} {
-        return "Sure."
-    }
+    if {$s eq ""} { return "Fine. Be that way!" }
+    set question [string match {*\?} $s]
+    set hasAlpha [regexp {[[:alpha:]]} $s]
+    set yelling [expr {$hasAlpha && $s eq [string toupper $s]}]
+    if {$yelling && $question} { return "Calm down, I know what I'm doing!" }
+    if {$yelling} { return "Whoa, chill out!" }
+    if {$question} { return "Sure." }
     return "Whatever."
 }
