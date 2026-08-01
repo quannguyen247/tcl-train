b262ff52e7253a5b04f469a4d50da90ed65d18d9
quannguyen247 <nguyendongquan247@gmail.com>
2026-08-01 14:02:27 +0700

    refactor(practices): improve flatten-array solution

--- practices/038-flatten-array/flatten-array.tcl
+++ practices/038-flatten-array/flatten-array.tcl
@@ -1,4 +1,19 @@
 proc flatten {input} {
-    return {}
-}
+    set result {}
 
+    foreach item $input {
+        if {$item eq ""} {
+            continue
+        }
+
+        set size [llength $item]
+
+        if {$size > 1 || ($size == 1 && [lindex $item 0] ne $item)} {
+            lappend result {*}[flatten $item]
+        } else {
+            lappend result $item
+        }
+    }
+
+    return $result
+}
