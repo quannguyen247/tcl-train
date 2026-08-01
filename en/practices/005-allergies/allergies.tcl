12455ec4e15e2071de4c65b469c1f472e2e13fab
quannguyen247 <nguyendongquan247@gmail.com>
2026-08-01 18:59:09 +0700

    Feat(practices): Complete solution for 005-allergies (vi & en)

--- en/practices/005-allergies/allergies.tcl
+++ en/practices/005-allergies/allergies.tcl
@@ -33,11 +33,33 @@
 # YOUR SOLUTION CODE BELOW
 # ==============================================================================
 
+set ALLERGENS {
+    eggs 1
+    peanuts 2
+    shellfish 4
+    strawberries 8
+    tomatoes 16
+    chocolate 32
+    pollen 64
+    cats 128
+}
+
 proc allergicTo {allergen score} {
-    throw {NOT_IMPLEMENTED} "Implement this procedure."
+    global ALLERGENS
+    if {![dict exists $ALLERGENS $allergen]} {
+        return false
+    }
+    set val [dict get $ALLERGENS $allergen]
+    return [expr {($score & $val) != 0}]
 }
 
 proc listAllergies {score} {
-    throw {NOT_IMPLEMENTED} "Implement this procedure."
+    global ALLERGENS
+    set result {}
+    dict for {item val} $ALLERGENS {
+        if {($score & $val) != 0} {
+            lappend result $item
+        }
+    }
+    return $result
 }
-
