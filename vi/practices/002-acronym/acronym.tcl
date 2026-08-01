9eb686131b089e771510e5d6725ed323dabb40ec
quannguyen247 <nguyendongquan247@gmail.com>
2026-08-01 18:59:09 +0700

    Feat(practices): Complete solution for 002-acronym (vi & en)

--- vi/practices/002-acronym/acronym.tcl
+++ vi/practices/002-acronym/acronym.tcl
@@ -24,6 +24,14 @@
 # ==============================================================================
 
 proc abbreviate {phrase} {
-    throw {NOT_IMPLEMENTED} "Implement this procedure."
+    set clean [regsub -all {[-_]} $phrase " "]
+    set words [split $clean " "]
+    set result ""
+    foreach word $words {
+        set word [string trim $word]
+        if {$word ne ""} {
+            append result [string toupper [string index $word 0]]
+        }
+    }
+    return $result
 }
-
