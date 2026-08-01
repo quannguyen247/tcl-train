ee1788625d1d44d57e6296837d6a6abde584d06d
quannguyen247 <nguyendongquan247@gmail.com>
2026-08-01 18:59:09 +0700

    Feat(practices): Complete solution for 004-all-your-base (vi & en)

--- en/practices/004-all-your-base/all-your-base.tcl
+++ en/practices/004-all-your-base/all-your-base.tcl
@@ -35,6 +35,19 @@
 # ==============================================================================
 
 proc rebase {inputBase digits outputBase} {
-    throw {NOT_IMPLEMENTED} "Implement this procedure."
+    if {$inputBase < 2} { error "input base must be >= 2" }
+    if {$outputBase < 2} { error "output base must be >= 2" }
+    set decimal 0
+    foreach d $digits {
+        if {$d < 0 || $d >= $inputBase} { error "all digits must satisfy 0 <= d < input base" }
+        set decimal [expr {$decimal * $inputBase + $d}]
+    }
+    if {$decimal == 0} { return {0} }
+    set res {}
+    while {$decimal > 0} {
+        set rem [expr {$decimal % $outputBase}]
+        set res [linsert $res 0 $rem]
+        set decimal [expr {$decimal / $outputBase}]
+    }
+    return $res
 }
-
