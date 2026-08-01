62cc4e872b6c49c54082103ceda3c96d1e81f4f1
quannguyen247 <nguyendongquan247@gmail.com>
2026-08-01 13:04:47 +0700

    refactor(practices): improve etl solution

--- practices/037-etl/etl.tcl
+++ practices/037-etl/etl.tcl
@@ -1,4 +1,12 @@
-proc transform {} {
-    return {}
+proc transform {legacy} {
+    if {[llength $legacy] % 2 != 0} {
+        error "invalid input"
+    }
+    set result [dict create]
+    dict for {score letters} $legacy {
+        foreach letter $letters {
+            dict set result [string tolower $letter] $score
+        }
+    }
+    return $result
 }
-
