589384060cd30809d7e7d7a80f73798152de0530
quannguyen247 <nguyendongquan247@gmail.com>
2026-08-01 18:59:10 +0700

    Feat(practices): Complete solution for 007-anagram (vi & en)

--- vi/practices/007-anagram/anagram.tcl
+++ vi/practices/007-anagram/anagram.tcl
@@ -19,6 +19,17 @@
 # ==============================================================================
 
 proc findAnagrams {subject candidates} {
-    throw {NOT_IMPLEMENTED} "Implement this procedure."
+    set sub_lower [string tolower $subject]
+    set sub_sorted [lsort [split $sub_lower ""]]
+    set result {}
+    foreach cand $candidates {
+        set cand_lower [string tolower $cand]
+        if {$cand_lower ne $sub_lower} {
+            set cand_sorted [lsort [split $cand_lower ""]]
+            if {$cand_sorted eq $sub_sorted} {
+                lappend result $cand
+            }
+        }
+    }
+    return $result
 }
-
