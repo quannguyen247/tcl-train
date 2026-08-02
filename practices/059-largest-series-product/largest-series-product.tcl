049dab8516ed1d525f848ec408ec69c58acd97bf
quannguyen247 <nguyendongquan247@gmail.com>
2026-08-02 17:09:32 +0700

    refactor(practices): improve largest-series-product solution

--- practices/059-largest-series-product/largest-series-product.tcl
+++ practices/059-largest-series-product/largest-series-product.tcl
@@ -1,4 +1,33 @@
 proc largestSeriesProduct {digits span} {
-    throw {NOT_IMPLEMENTED} "Implement this procedure."
-}
+    set len [string length $digits]
 
+    if {$span < 0} {
+        error "span must not be negative"
+    }
+    if {$span > $len} {
+        error "span must not exceed string length"
+    }
+    if {$digits ne "" && ![regexp {^[0-9]+$} $digits]} {
+        error "digits input must only contain digits"
+    }
+    if {$span == 0} {
+        return 1
+    }
+
+    set max_prod 0
+
+    for {set i 0} {$i <= $len - $span} {incr i} {
+        set sub [string range $digits $i [expr {$i + $span - 1}]]
+
+        set prod 1
+        foreach d [split $sub ""] {
+            set prod [expr {$prod * $d}]
+        }
+
+        if {$prod > $max_prod} {
+            set max_prod $prod
+        }
+    }
+
+    return $max_prod
+}
