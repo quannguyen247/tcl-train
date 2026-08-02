6a5534f44fdb61b85e44a1f217074ca19ed4650e
quannguyen247 <nguyendongquan247@gmail.com>
2026-08-02 15:55:39 +0700

    refactor(practices): improve knapsack solution

--- practices/058-knapsack/knapsack.tcl
+++ practices/058-knapsack/knapsack.tcl
@@ -1,4 +1,16 @@
 proc maximumValue {maxWeight items} {
-    throw {NOT_IMPLEMENTED} "Implement this procedure."
-}
+    set best [lrepeat [expr {$maxWeight + 1}] 0]
 
+    foreach item $items {
+        dict with item {}
+
+        for {set w $maxWeight} {$w >= $weight} {incr w -1} {
+            set total [expr {[lindex $best [expr {$w - $weight}]] + $value}]
+            if {$total > [lindex $best $w]} {
+                lset best $w $total
+            }
+        }
+    }
+
+    return [lindex $best end]
+}
