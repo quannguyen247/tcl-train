4fded1c2e25db9dcada58f6d9f6dd58fcc70690c
quannguyen247 <nguyendongquan247@gmail.com>
2026-08-02 11:55:12 +0700

    refactor(practices): improve isogram solution

--- practices/055-isogram/isogram.tcl
+++ practices/055-isogram/isogram.tcl
@@ -1,4 +1,18 @@
 proc isIsogram {input} {
-    return {}
-}
+    set seen {}
 
+    foreach char [split [string tolower $input] ""] {
+
+        if {$char eq " " || $char eq "-"} {
+            continue
+        }
+
+        if {[dict exists $seen $char]} {
+            return false
+        }
+
+        dict set seen $char 1
+    }
+
+    return true
+}
