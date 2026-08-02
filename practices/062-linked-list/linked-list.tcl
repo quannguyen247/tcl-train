70281afde905f322497b3f0a7c900dd5b84da7c5
quannguyen247 <nguyendongquan247@gmail.com>
2026-08-02 20:39:54 +0700

    refactor(practices): improve linked-list solution

--- practices/062-linked-list/linked-list.tcl
+++ practices/062-linked-list/linked-list.tcl
@@ -1,28 +1,36 @@
 oo::class create LinkedList {
-    constructor {} {
-        throw {NOT_IMPLEMENTED} "Implement this class."
-    }
+    variable list
 
-    method push {value} {
-    }
+    constructor {} { set list {} }
 
-    method unshift {value} {
-    }
+    method push {value} { lappend list $value }
+
+    method unshift {value} { set list [linsert $list 0 $value] }
 
     method pop {} {
+        set val [lindex $list end]
+        set list [lrange $list 0 end-1]
+        return $val
     }
 
     method shift {} {
+        set val [lindex $list 0]
+        set list [lrange $list 1 end]
+        return $val
     }
 
-    method length {} {
-    }
+    method length {} { return [llength $list] }
 
     method delete {value} {
+        set idx [lsearch -exact $list $value]
+        if {$idx != -1} { set list [lreplace $list $idx $idx] }
     }
 
-    # bonus
     method for {varname script} {
+        upvar 1 $varname var
+        foreach val $list {
+            set var $val
+            uplevel 1 $script
+        }
     }
 }
-
