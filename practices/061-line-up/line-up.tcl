a002e26df4bc512f473198218544e74c0a076fd0
quannguyen247 <nguyendongquan247@gmail.com>
2026-08-02 19:26:29 +0700

    refactor(practices): improve line-up solution

--- practices/061-line-up/line-up.tcl
+++ practices/061-line-up/line-up.tcl
@@ -1,4 +1,22 @@
 proc formatTicket {name number} {
-    throw {NOT_IMPLEMENTED} "Implement this procedure."
-}
 
+    set last2 [expr {$number % 100}]
+
+    if {$last2 == 11 || $last2 == 12 || $last2 == 13} {
+        set suffix "th"
+    } else {
+        set last [expr {$number % 10}]
+
+        if {$last == 1} {
+            set suffix "st"
+        } elseif {$last == 2} {
+            set suffix "nd"
+        } elseif {$last == 3} {
+            set suffix "rd"
+        } else {
+            set suffix "th"
+        }
+    }
+
+    return "$name, you are the ${number}${suffix} customer we serve today. Thank you!"
+}
