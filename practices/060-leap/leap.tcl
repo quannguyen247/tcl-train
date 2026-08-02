3ab8227707adf4e6728e94459dbbada95c7aa336
quannguyen247 <nguyendongquan247@gmail.com>
2026-08-02 18:09:19 +0700

    refactor(practices): improve leap solution

--- practices/060-leap/leap.tcl
+++ practices/060-leap/leap.tcl
@@ -1,4 +1,15 @@
 proc isLeapYear {year} {
-    throw {NOT_IMPLEMENTED} "Implement this procedure."
-}
+    if {$year % 400 == 0} {
+        return 1
+    }
 
+    if {$year % 100 == 0} {
+        return 0
+    }
+
+    if {$year % 4 == 0} {
+        return 1
+    }
+
+    return 0
+}
