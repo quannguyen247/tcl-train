24aa6da7a47ee2cec87e5b338f1dddf501e05535
quannguyen247 <nguyendongquan247@gmail.com>
2026-08-02 13:17:54 +0700

    refactor(practices): improve killer-sudoku-helper solution

--- practices/056-killer-sudoku-helper/killer-sudoku-helper.tcl
+++ practices/056-killer-sudoku-helper/killer-sudoku-helper.tcl
@@ -1,4 +1,20 @@
-proc combinations {sum size exclude} {
-    return {}
+proc cageSearch {sum size exclude start chosen} {
+    if {$size == 0} {
+        return [expr {$sum == 0 ? [list $chosen] : {}}]
+    }
+
+    set result {}
+    for {set digit $start} {$digit <= 9} {incr digit} {
+        if {$digit in $exclude || $digit > $sum} {
+            continue
+        }
+        lappend result {*}[cageSearch [expr {$sum - $digit}] \
+            [expr {$size - 1}] $exclude [expr {$digit + 1}] \
+            [concat $chosen $digit]]
+    }
+    return $result
 }
 
+proc combinations {sum size exclude} {
+    return [cageSearch $sum $size $exclude 1 {}]
+}
