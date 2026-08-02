7c74f7a44b5ef91a974aee51cb4e19153cbaf30b
quannguyen247 <nguyendongquan247@gmail.com>
2026-08-02 14:56:59 +0700

    Feat(practices): Complete solution for 014-binary-search-tree (vi & en)

--- en/practices/014-binary-search-tree/binary-search-tree.tcl
+++ en/practices/014-binary-search-tree/binary-search-tree.tcl
@@ -77,14 +77,67 @@
 # ==============================================================================
 
 oo::class create BinarySearchTree {
-    variable data
-    variable left
-    variable right
+    variable value
+    variable leftNode
+    variable rightNode
 
-    method insert {value} {
-        throw {NOT_IMPLEMENTED} "Implement this method."
+    constructor {} {
+        set value ""
+        set leftNode ""
+        set rightNode ""
     }
 
-    # other methods need to be implemented too...
-}
+    method data {} {
+        return $value
+    }
 
+    method insert {val} {
+        if {$value eq ""} {
+            set value $val
+        } elseif {$val <= $value} {
+            if {$leftNode eq ""} {
+                set leftNode [BinarySearchTree new]
+            }
+            $leftNode insert $val
+        } else {
+            if {$rightNode eq ""} {
+                set rightNode [BinarySearchTree new]
+            }
+            $rightNode insert $val
+        }
+    }
+
+    method toDict {} {
+        if {$value eq ""} {
+            return {}
+        }
+        set ldict [expr {$leftNode eq "" ? {} : [$leftNode toDict]}]
+        set rdict [expr {$rightNode eq "" ? {} : [$rightNode toDict]}]
+        return [dict create data $value left $ldict right $rdict]
+    }
+
+    method sorted {} {
+        if {$value eq ""} {
+            return {}
+        }
+        set llist [expr {$leftNode eq "" ? {} : [$leftNode sorted]}]
+        set rlist [expr {$rightNode eq "" ? {} : [$rightNode sorted]}]
+        return [concat $llist [list $value] $rlist]
+    }
+
+    method map {varname body} {
+        upvar 1 $varname var
+        set res {}
+        if {$leftNode ne ""} {
+            set res [concat $res [$leftNode map $varname $body]]
+        }
+        if {$value ne ""} {
+            set var [self]
+            lappend res [uplevel 1 $body]
+        }
+        if {$rightNode ne ""} {
+            set res [concat $res [$rightNode map $varname $body]]
+        }
+        return $res
+    }
+}
