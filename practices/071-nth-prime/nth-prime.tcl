c4ffd0d3f2067da6ffa3d0687ac89249de53e2fa
quannguyen247 <nguyendongquan247@gmail.com>
2026-08-03 14:13:55 +0700

    refactor(practices): improve nth-prime solution

--- practices/071-nth-prime/nth-prime.tcl
+++ practices/071-nth-prime/nth-prime.tcl
@@ -1,4 +1,15 @@
-proc nthPrime {idx} {
-    throw {NOT_IMPLEMENTED} "Implement this procedure."
+proc nthPrime {n} {
+    if {$n <= 0} { return -code error "there is no zeroth prime" }
+    set primes {}
+    set i 2
+    while {[llength $primes] < $n} {
+        set isPrime 1
+        foreach p $primes {
+            if {$p * $p > $i} break
+            if {$i % $p == 0} { set isPrime 0; break }
+        }
+        if {$isPrime} { lappend primes $i }
+        incr i
+    }
+    return [lindex $primes end]
 }
-
