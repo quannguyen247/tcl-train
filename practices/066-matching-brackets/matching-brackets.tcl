59c7702104f2a2aad2959fe83141436754ca259d
quannguyen247 <nguyendongquan247@gmail.com>
2026-08-03 10:28:36 +0700

    refactor(practices): improve matching-brackets solution

--- practices/066-matching-brackets/matching-brackets.tcl
+++ practices/066-matching-brackets/matching-brackets.tcl
@@ -1,4 +1,31 @@
 proc bracketsMatch {input} {
-    throw {NOT_IMPLEMENTED} "Implement this procedure."
-}
+    set opens "\(\[\{"
+    set closes "\)\]\}"
+    set stack {}
 
+    foreach char [split $input ""] {
+        set index [string first $char $opens]
+
+        if {$index >= 0} {
+            lappend stack $char
+            continue
+        }
+
+        set index [string first $char $closes]
+
+        if {$index < 0} {
+            continue
+        }
+
+        set lastOpen [lindex $stack end]
+        set expectedOpen [string index $opens $index]
+
+        if {$stack eq {} || $lastOpen ne $expectedOpen} {
+            return false
+        }
+
+        set stack [lrange $stack 0 end-1]
+    }
+
+    return [expr {$stack eq {}}]
+}
