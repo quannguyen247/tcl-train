815a146cccb6b5743ed9ecf94f953852cd86149b
quannguyen247 <nguyendongquan247@gmail.com>
2026-08-03 09:35:49 +0700

    refactor(practices): improve luhn solution

--- practices/064-luhn/luhn.tcl
+++ practices/064-luhn/luhn.tcl
@@ -1,5 +1,25 @@
 proc luhn {digits} {
-    throw {NOT_IMPLEMENTED} "Implement this procedure."
+    set cleaned_digits [string map {" " ""} $digits]
+
+    if {[string length $cleaned_digits] <= 1 || ![regexp {^[0-9]+$} $cleaned_digits]} {
+        return false
+    }
+
+    set rev [string reverse $cleaned_digits]
+    set sum 0
+
+    for {set i 0} {$i < [string length $rev]} {incr i} {
+        set d [string index $rev $i]
+
+        if {$i % 2 == 1} {
+            set d [expr {$d * 2}]
+            if {$d > 9} {
+                set d [expr {$d - 9}]
+            }
+        }
+
+        incr sum $d
+    }
+
+    return [expr {$sum % 10 == 0}]
 }
-
-
