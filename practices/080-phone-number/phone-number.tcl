ce32a0fa353a3dd309c7ee544fc835ee96477ac8
quannguyen247 <nguyendongquan247@gmail.com>
2026-08-04 15:26:05 +0700

    refactor(practices): improve phone-number solution

--- practices/080-phone-number/phone-number.tcl
+++ practices/080-phone-number/phone-number.tcl
@@ -1,4 +1,34 @@
 proc clean {phone} {
-    throw {NOT_IMPLEMENTED} "Implement this procedure."
-}
+    if {[regexp {[a-zA-Z]} $phone]} {
+        return -code error "letters not permitted"
+    }
+    if {[regexp {[^0-9\s\.\-\+\(\)]} $phone]} {
+        return -code error "punctuations not permitted"
+    }
+    regsub -all {[^0-9]} $phone "" phone
 
+    set len [string length $phone]
+    if {$len < 10} {
+        return -code error "must not be fewer than 10 digits"
+    } elseif {$len > 11} {
+        return -code error "must not be greater than 11 digits"
+    } elseif {$len == 11} {
+        if {[string index $phone 0] ne "1"} {
+            return -code error "11 digits must start with 1"
+        }
+        set phone [string range $phone 1 end]
+    }
+
+    if {[string index $phone 0] eq "0"} {
+        return -code error "area code cannot start with zero"
+    } elseif {[string index $phone 0] eq "1"} {
+        return -code error "area code cannot start with one"
+    }
+    if {[string index $phone 3] eq "0"} {
+        return -code error "exchange code cannot start with zero"
+    } elseif {[string index $phone 3] eq "1"} {
+        return -code error "exchange code cannot start with one"
+    }
+
+    return $phone
+}
