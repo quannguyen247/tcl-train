d459ef4f5f421b3986e6e80be99c9c799a4dc1f7
quannguyen247 <nguyendongquan247@gmail.com>
2026-08-04 13:04:39 +0700

    refactor(practices): improve pascals-triangle solution

--- practices/078-pascals-triangle/pascals-triangle.tcl
+++ practices/078-pascals-triangle/pascals-triangle.tcl
@@ -1,4 +1,15 @@
 proc triangle {n} {
-    throw {NOT_IMPLEMENTED} "Implement this procedure."
+    set res {}
+    for {set i 0} {$i < $n} {incr i} {
+        set row {1}
+        set prev [lindex $res end]
+        if {[llength $prev] > 0} {
+            for {set j 0} {$j < [llength $prev]-1} {incr j} {
+                lappend row [expr {[lindex $prev $j] + [lindex $prev $j+1]}]
+            }
+            lappend row 1
+        }
+        lappend res $row
+    }
+    return $res
 }
-
