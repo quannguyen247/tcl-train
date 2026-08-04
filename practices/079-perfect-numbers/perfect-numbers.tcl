2c63cf2e0eadc58802eac6f21f5e74c3e744576d
quannguyen247 <nguyendongquan247@gmail.com>
2026-08-04 14:20:17 +0700

    refactor(practices): improve perfect-numbers solution

--- practices/079-perfect-numbers/perfect-numbers.tcl
+++ practices/079-perfect-numbers/perfect-numbers.tcl
@@ -1,4 +1,12 @@
-proc classify {} {
-    throw {NOT_IMPLEMENTED} "Implement this procedure."
+proc classify {number} {
+    if {$number <= 0} {
+        error "Classification is only possible for positive integers."
+    }
+    set sum 0
+    for {set i 1} {$i <= $number / 2} {incr i} {
+        if {$number % $i == 0} { incr sum $i }
+    }
+    if {$sum == $number} { return "perfect" }
+    if {$sum > $number} { return "abundant" }
+    return "deficient"
 }
-
