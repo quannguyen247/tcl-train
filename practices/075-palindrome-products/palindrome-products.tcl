f4cb809051147cbe9f5c71530cbe2c3e9fa1912c
quannguyen247 <nguyendongquan247@gmail.com>
2026-08-04 10:51:09 +0700

    refactor(practices): improve palindrome-products solution

--- practices/075-palindrome-products/palindrome-products.tcl
+++ practices/075-palindrome-products/palindrome-products.tcl
@@ -1,4 +1,66 @@
-proc palindromeProducts {smallestOrLargest min max} {
-    throw {NOT_IMPLEMENTED} "Implement this procedure."
+proc isPalindrome {number} {
+    return [expr {$number eq [string reverse $number]}]
 }
 
+proc palindromeProducts {smallestOrLargest min max} {
+    if {$min > $max} {
+        error "min must be <= max"
+    }
+
+    set best -1
+    set factors {}
+
+    if {$smallestOrLargest eq "smallest"} {
+        set step 1
+        set start $min
+        set stop $max
+    } else {
+        set step -1
+        set start $max
+        set stop $min
+    }
+
+    for {set first $start} {
+        ($step > 0 && $first <= $stop)
+        || ($step < 0 && $first >= $stop)
+    } {incr first $step} {
+        if {$smallestOrLargest eq "smallest"} {
+            set secondStart $first
+            set secondStop $max
+            set secondStep 1
+        } else {
+            set secondStart $max
+            set secondStop $first
+            set secondStep -1
+        }
+
+        for {set second $secondStart} {
+            ($secondStep > 0 && $second <= $secondStop)
+            || ($secondStep < 0 && $second >= $secondStop)
+        } {incr second $secondStep} {
+            set product [expr {$first * $second}]
+
+            if {$best >= 0
+                && (($step > 0 && $product > $best)
+                    || ($step < 0 && $product < $best))} {
+                break
+            }
+
+            if {![isPalindrome $product]} {
+                continue
+            }
+
+            if {$best < 0 || ($step > 0 && $product < $best)
+                || ($step < 0 && $product > $best)} {
+                set best $product
+                set factors {}
+            }
+
+            if {$product == $best} {
+                lappend factors [list $first $second]
+            }
+        }
+    }
+
+    return [list $best [lsort -integer -index 0 $factors]]
+}
