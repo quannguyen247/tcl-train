e4b11e8c4df50917df18c0284ab7c93001558e9d
quannguyen247 <nguyendongquan247@gmail.com>
2026-08-04 17:38:55 +0700

    refactor(practices): improve pig-latin solution

--- practices/082-pig-latin/pig-latin.tcl
+++ practices/082-pig-latin/pig-latin.tcl
@@ -1,4 +1,23 @@
-proc translate {sentence} {
-    throw {NOT_IMPLEMENTED} "Implement this procedure."
+proc translateWord {word} {
+    if {[regexp {^([aeiou]|xr|yt)} $word]} {
+        return "${word}ay"
+    }
+    if {[regexp {^([^aeiou]*qu)(.*)} $word match cons rest]} {
+        return "${rest}${cons}ay"
+    }
+    if {[regexp {^([^aeiou]+)(y.*)} $word match cons rest]} {
+        return "${rest}${cons}ay"
+    }
+    if {[regexp {^([^aeiou]+)(.*)} $word match cons rest]} {
+        return "${rest}${cons}ay"
+    }
+    return $word
 }
 
+proc translate {sentence} {
+    set out {}
+    foreach w [split $sentence] {
+        lappend out [translateWord $w]
+    }
+    return [join $out " "]
+}
