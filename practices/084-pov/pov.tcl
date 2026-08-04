49ed0e982c5f550e9bcddde67e2fec0e980aa082
quannguyen247 <nguyendongquan247@gmail.com>
2026-08-04 19:22:16 +0700

    refactor(practices): improve pov solution

--- practices/084-pov/pov.tcl
+++ practices/084-pov/pov.tcl
@@ -1,8 +1,62 @@
+proc findNodePath {tree label} {
+    if {[dict get $tree label] eq $label} {
+        return [list $tree]
+    }
+
+    if {[dict exists $tree children]} {
+        foreach child [dict get $tree children] {
+            set found [findNodePath $child $label]
+            if {$found ne {}} {
+                return [linsert $found 0 $tree]
+            }
+        }
+    }
+    return {}
+}
+
 proc fromPov {treeInput label} {
-    throw {NOT_IMPLEMENTED} "Implement this procedure."
+    set nodes [findNodePath $treeInput $label]
+    if {$nodes eq {}} {
+        error "no such target"
+    }
+
+    set result {}
+    for {set i 0} {$i < [llength $nodes]} {incr i} {
+        set node [lindex $nodes $i]
+
+        if {$i < [llength $nodes] - 1} {
+            set nextLabel [dict get [lindex $nodes [expr {$i + 1}]] label]
+            set children {}
+
+            foreach child [dict get $node children] {
+                if {[dict get $child label] ne $nextLabel} {
+                    lappend children $child
+                }
+            }
+
+            if {$children eq {}} {
+                dict unset node children
+            } else {
+                dict set node children $children
+            }
+        }
+
+        if {$result ne {}} {
+            dict lappend node children $result
+        }
+        set result $node
+    }
+    return $result
 }
 
 proc path {treeInput fromLabel toLabel} {
-    throw {NOT_IMPLEMENTED} "Implement this procedure."
-}
+    if {[catch {set tree [fromPov $treeInput $fromLabel]}]} {
+        error "no such label"
+    }
 
+    set nodes [findNodePath $tree $toLabel]
+    if {$nodes eq {}} {
+        error "no such label"
+    }
+    return [lmap node $nodes {dict get $node label}]
+}
