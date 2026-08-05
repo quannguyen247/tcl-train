85e020f76d0934f1ad1ea0118baf6d6859855286
quannguyen247 <nguyendongquan247@gmail.com>
2026-08-05 12:50:06 +0700

    refactor(practices): improve pythagorean-triplet solution

--- practices/089-pythagorean-triplet/pythagorean-triplet.tcl
+++ practices/089-pythagorean-triplet/pythagorean-triplet.tcl
@@ -1,4 +1,21 @@
 proc tripletsWithSum {perimeter} {
-    throw {NOT_IMPLEMENTED} "Implement this procedure."
-}
+    set result {}
 
+    for {set a 1} {$a < $perimeter / 3} {incr a} {
+        set numerator [expr {$perimeter * ($perimeter - 2 * $a)}]
+        set denominator [expr {2 * ($perimeter - $a)}]
+
+        if {$numerator % $denominator} {
+            continue
+        }
+
+        set b [expr {$numerator / $denominator}]
+        set c [expr {$perimeter - $a - $b}]
+
+        if {$a < $b && $b < $c} {
+            lappend result [list $a $b $c]
+        }
+    }
+
+    return $result
+}
