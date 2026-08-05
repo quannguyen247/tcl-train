70cb44f891819a87e80b440b0ae0fa710d6ec23a
quannguyen247 <nguyendongquan247@gmail.com>
2026-08-05 08:42:31 +0700

    refactor(practices): improve prime-factors solution

--- practices/085-prime-factors/prime-factors.tcl
+++ practices/085-prime-factors/prime-factors.tcl
@@ -1,4 +1,16 @@
 proc factors {n} {
-    throw {NOT_IMPLEMENTED} "Implement this procedure."
+    set res {}
+    set f 2
+    while {$n > 1} {
+        while {$n % $f == 0} {
+            lappend res $f
+            set n [expr {$n / $f}]
+        }
+        incr f
+        if {$f * $f > $n && $n > 1} {
+            lappend res $n
+            break
+        }
+    }
+    return $res
 }
-
