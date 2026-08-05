e552cf68744652113cd6bebbf7d0e80c5391a198
quannguyen247 <nguyendongquan247@gmail.com>
2026-08-05 10:37:19 +0700

    refactor(practices): improve protein-translation solution

--- practices/087-protein-translation/protein-translation.tcl
+++ practices/087-protein-translation/protein-translation.tcl
@@ -1,4 +1,17 @@
 proc proteins {strand} {
-    throw {NOT_IMPLEMENTED} "Implement this procedure."
+    set res {}
+    foreach {a b c} [split $strand ""] {
+        switch -exact -- "$a$b$c" {
+            AUG { lappend res "Methionine" }
+            UUU - UUC { lappend res "Phenylalanine" }
+            UUA - UUG { lappend res "Leucine" }
+            UCU - UCC - UCA - UCG { lappend res "Serine" }
+            UAU - UAC { lappend res "Tyrosine" }
+            UGU - UGC { lappend res "Cysteine" }
+            UGG { lappend res "Tryptophan" }
+            UAA - UAG - UGA { break }
+            default { return -code error "Invalid codon" }
+        }
+    }
+    return $res
 }
-
