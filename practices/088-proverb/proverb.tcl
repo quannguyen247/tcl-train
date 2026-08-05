74ebe5eb60f8adafa9d653c2ecc4dd7e7c426e4f
quannguyen247 <nguyendongquan247@gmail.com>
2026-08-05 11:44:21 +0700

    refactor(practices): improve proverb solution

--- practices/088-proverb/proverb.tcl
+++ practices/088-proverb/proverb.tcl
@@ -1,4 +1,11 @@
 proc recite {items} {
-    throw {NOT_IMPLEMENTED} "Implement this procedure."
+    if {[llength $items] == 0} { return {} }
+    set res {}
+    for {set i 0} {$i < [llength $items] - 1} {incr i} {
+        set curr [lindex $items $i]
+        set next [lindex $items [expr {$i + 1}]]
+        lappend res "For want of a $curr the $next was lost."
+    }
+    lappend res "And all for the want of a [lindex $items 0]."
+    return $res
 }
-
