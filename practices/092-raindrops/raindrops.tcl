fe9ac839ce74e90c7a84a59bcbafb2afca6ba67e
quannguyen247 <nguyendongquan247@gmail.com>
2026-08-06 08:37:40 +0700

    refactor(practices): improve raindrops solution

--- practices/092-raindrops/raindrops.tcl
+++ practices/092-raindrops/raindrops.tcl
@@ -1,4 +1,15 @@
 proc raindrops {number} {
-    throw {NOT_IMPLEMENTED} "Implement this procedure."
-}
+    set result ""
 
+    foreach {factor sound} {3 Pling 5 Plang 7 Plong} {
+        if {$number % $factor == 0} {
+            append result $sound
+        }
+    }
+
+    if {$result eq ""} {
+        return $number
+    }
+
+    return $result
+}
