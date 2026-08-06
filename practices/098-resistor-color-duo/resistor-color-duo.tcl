c4e6d5e0638cb3a5b779ffac0074806235cdfca7
quannguyen247 <nguyendongquan247@gmail.com>
2026-08-06 14:59:33 +0700

    refactor(practices): improve resistor-color-duo solution

--- practices/098-resistor-color-duo/resistor-color-duo.tcl
+++ practices/098-resistor-color-duo/resistor-color-duo.tcl
@@ -1,6 +1,19 @@
 namespace eval ::resistorColor {
-    proc value {} {
-        throw {NOT_IMPLEMENTED} "Implement this procedure."
+    variable colors {
+        black brown red orange yellow
+        green blue violet grey white
+    }
+
+    proc value {first second args} {
+        variable colors
+
+        set firstValue [lsearch -exact $colors $first]
+        set secondValue [lsearch -exact $colors $second]
+
+        if {$firstValue < 0 || $secondValue < 0} {
+            error "Invalid color"
+        }
+
+        return [expr {$firstValue * 10 + $secondValue}]
     }
 }
-
