726484bae410d1d10f1cd17f31ed3ecbea6c3430
quannguyen247 <nguyendongquan247@gmail.com>
2026-08-06 19:56:36 +0700

    Feat(practices): Complete solution for 091-rail-fence-cipher

--- vi/practices/091-rail-fence-cipher/rail-fence-cipher.tcl
+++ vi/practices/091-rail-fence-cipher/rail-fence-cipher.tcl
@@ -67,12 +67,61 @@ namespace eval railFenceCipher {
     namespace export encode decode
     namespace ensemble create
 
+    proc pattern {length rails} {
+        set result {}
+        set rail 0
+        set direction 1
+
+        # Tạo thứ tự rail theo đường zig-zag
+        for {set i 0} {$i < $length} {incr i} {
+            lappend result $rail
+
+            if {$rail == 0} {
+                set direction 1
+            } elseif {$rail == $rails - 1} {
+                set direction -1
+            }
+            incr rail $direction
+        }
+        return $result
+    }
+
     proc encode {phrase num} {
-        throw {NOT_IMPLEMENTED} "Implement this procedure."
+        set rows [lrepeat $num ""]
+
+        # Đưa từng ký tự vào rail tương ứng
+        foreach char [split $phrase ""] rail [pattern [string length $phrase] $num] {
+            lset rows $rail "[lindex $rows $rail]$char"
+        }
+        return [join $rows ""]
     }
 
     proc decode {cipher num} {
-        throw {NOT_IMPLEMENTED} "Implement this procedure."
+        set route [pattern [string length $cipher] $num]
+        set counts [lrepeat $num 0]
+
+        foreach rail $route {
+            lset counts $rail [expr {[lindex $counts $rail] + 1}]
+        }
+
+        # Chia cipher thành nội dung của từng rail
+        set rows {}
+        set start 0
+        foreach count $counts {
+            lappend rows [string range $cipher \
+                $start [expr {$start + $count - 1}]]
+            incr start $count
+        }
+
+        # Đọc lại các ký tự theo đường zig-zag
+        set positions [lrepeat $num 0]
+        set result ""
+        foreach rail $route {
+            set index [lindex $positions $rail]
+            append result [string index [lindex $rows $rail] $index]
+            lset positions $rail [expr {$index + 1}]
+        }
+        return $result
     }
 }
 
