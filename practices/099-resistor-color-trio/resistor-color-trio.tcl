d7bcd9ed343f12207326612ead93e66eeeed21f9
quannguyen247 <nguyendongquan247@gmail.com>
2026-08-07 08:33:42 +0700

    refactor(practices): improve resistor-color-trio solution

--- practices/099-resistor-color-trio/resistor-color-trio.tcl
+++ practices/099-resistor-color-trio/resistor-color-trio.tcl
@@ -1,6 +1,27 @@
 namespace eval ::resistorColor {
-    proc label {} {
-        throw {NOT_IMPLEMENTED} "Implement this procedure."
+    proc label {args} {
+        set colors {
+            black brown red orange yellow
+            green blue violet grey white
+        }
+
+        set digits [lmap color [lrange $args 0 2] {
+            lsearch -exact $colors $color
+        }]
+
+        lassign $digits first second zeros
+        set value [expr {
+            ($first * 10 + $second) * 10 ** $zeros
+        }]
+
+        set units {ohms kiloohms megaohms gigaohms}
+        set unit 0
+
+        while {$value >= 1000 && $value % 1000 == 0} {
+            set value [expr {$value / 1000}]
+            incr unit
+        }
+
+        return "$value [lindex $units $unit]"
     }
 }
-
