03d4082a6c7963e69e7f3b90562ec17ebea2a306
quannguyen247 <nguyendongquan247@gmail.com>
2026-08-07 14:50:21 +0700

    refactor(practices): improve rotational-cipher solution

--- practices/106-rotational-cipher/rotational-cipher.tcl
+++ practices/106-rotational-cipher/rotational-cipher.tcl
@@ -1,4 +1,18 @@
 proc rotate {text shift} {
-    throw {NOT_IMPLEMENTED} "Implement this procedure."
-}
+    set shift [expr {$shift % 26}]
+    set mapping {}
 
+    foreach alphabet {
+        abcdefghijklmnopqrstuvwxyz
+        ABCDEFGHIJKLMNOPQRSTUVWXYZ
+    } {
+        set rotated [string range "$alphabet$alphabet" \
+            $shift [expr {$shift + 25}]]
+
+        foreach old [split $alphabet ""] new [split $rotated ""] {
+            lappend mapping $old $new
+        }
+    }
+
+    return [string map $mapping $text]
+}
