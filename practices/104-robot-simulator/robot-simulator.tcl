d160335da395238e654a132bdf8d811d6142e1d1
quannguyen247 <nguyendongquan247@gmail.com>
2026-08-07 12:50:00 +0700

    refactor(practices): improve robot-simulator solution

--- practices/104-robot-simulator/robot-simulator.tcl
+++ practices/104-robot-simulator/robot-simulator.tcl
@@ -1,6 +1,58 @@
 oo::class create Robot {
+    variable x y direction
+
     constructor {args} {
-        throw {NOT_IMPLEMENTED} "Implement this class."
+        set config [dict merge \
+            {x 0 y 0 direction north} {*}$args]
+
+        set x [dict get $config x]
+        set y [dict get $config y]
+        set direction [dict get $config direction]
+    }
+
+    method position {} {
+        return [dict create \
+            x $x y $y direction $direction]
+    }
+
+    method move {instructions} {
+        set directions {north east south west}
+        set steps {
+            north {0 1}
+            east {1 0}
+            south {0 -1}
+            west {-1 0}
+        }
+
+        foreach instruction [split $instructions ""] {
+            set index [lsearch -exact \
+                $directions $direction]
+
+            switch -- $instruction {
+                R {
+                    set direction [lindex $directions \
+                        [expr {($index + 1) % 4}]]
+                }
+
+                L {
+                    set direction [lindex $directions \
+                        [expr {($index + 3) % 4}]]
+                }
+
+                A {
+                    lassign [dict get \
+                        $steps $direction] dx dy
+
+                    incr x $dx
+                    incr y $dy
+                }
+
+                default {
+                    error "invalid instruction: $instruction"
+                }
+            }
+        }
+
+        return [my position]
     }
 }
-
