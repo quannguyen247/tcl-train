2ddf879a88dff13d8c10792cceb9fabf783d831e
quannguyen247 <nguyendongquan247@gmail.com>
2026-08-07 13:36:51 +0700

    refactor(practices): improve roman-numerals solution

--- practices/105-roman-numerals/roman-numerals.tcl
+++ practices/105-roman-numerals/roman-numerals.tcl
@@ -1,4 +1,17 @@
 proc toroman {n} {
-    throw {NOT_IMPLEMENTED} "Implement this procedure."
-}
+    set roman ""
+    set symbols {
+        1000 M 900 CM 500 D 400 CD
+        100 C 90 XC 50 L 40 XL
+        10 X 9 IX 5 V 4 IV 1 I
+    }
 
+    foreach {value symbol} $symbols {
+        set count [expr {$n / $value}]
+
+        append roman [string repeat $symbol $count]
+        set n [expr {$n % $value}]
+    }
+
+    return $roman
+}
