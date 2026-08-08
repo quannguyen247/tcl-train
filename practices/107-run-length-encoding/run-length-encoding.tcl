6b3e182bb1b824dd299fe2c4f3bc37a5d61068b5
quannguyen247 <nguyendongquan247@gmail.com>
2026-08-08 08:47:24 +0700

    refactor(practices): improve run-length-encoding solution

--- practices/107-run-length-encoding/run-length-encoding.tcl
+++ practices/107-run-length-encoding/run-length-encoding.tcl
@@ -1,8 +1,31 @@
-proc encode {string} {
-    throw {NOT_IMPLEMENTED} "Implement this procedure."
+proc encode {text} {
+    set res ""
+    set count 0; set current ""
+    foreach char [split $text ""] {
+        if {$char eq $current} {
+            incr count
+        } else {
+            if {$count > 1} { append res $count }
+            if {$count > 0} { append res $current }
+            set current $char; set count 1
+        }
+    }
+    if {$count > 1} { append res $count }
+    if {$count > 0} { append res $current }
+    return $res
 }
 
-proc decode {code} {
-    throw {NOT_IMPLEMENTED} "Implement this procedure."
+proc decode {text} {
+    set res ""
+    set count ""
+    foreach char [split $text ""] {
+        if {[string is integer $char]} {
+            append count $char
+        } else {
+            if {$count eq ""} { set count 1 }
+            append res [string repeat $char $count]
+            set count ""
+        }
+    }
+    return $res
 }
-
