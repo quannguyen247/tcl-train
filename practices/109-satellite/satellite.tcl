e673c67b727192689ab662360bf5d0c8b044ca10
quannguyen247 <nguyendongquan247@gmail.com>
2026-08-08 10:51:56 +0700

    refactor(practices): improve satellite solution

--- practices/109-satellite/satellite.tcl
+++ practices/109-satellite/satellite.tcl
@@ -1,4 +1,26 @@
 proc treeFromTraversals {preorder inorder} {
-    throw {NOT_IMPLEMENTED} "Implement this procedure."
-}
+    if {[llength $preorder] != [llength $inorder]} {
+        error "traversals must have the same length"
+    }
+    if {[llength [lsort -unique $preorder]] != [llength $preorder]} {
+        error "traversals must contain unique elements"
+    }
+    if {[lsort $preorder] ne [lsort $inorder]} {
+        error "traversals must contain the same elements"
+    }
 
+    if {$preorder eq {}} {
+        return {}
+    }
+
+    set root [lindex $preorder 0]
+    set split [lsearch -exact $inorder $root]
+    set leftInorder [lrange $inorder 0 [expr {$split - 1}]]
+    set rightInorder [lrange $inorder [expr {$split + 1}] end]
+    set leftSize [llength $leftInorder]
+
+    set left [treeFromTraversals [lrange $preorder 1 $leftSize] $leftInorder]
+    set right [treeFromTraversals [lrange $preorder [expr {$leftSize + 1}] end] $rightInorder]
+
+    return [dict create v $root l $left r $right]
+}
