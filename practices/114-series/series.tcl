7bb9012b13aa0c34e9cc47ba3de608c1cee17580
quannguyen247 <nguyendongquan247@gmail.com>
2026-08-08 15:05:17 +0700

    refactor(practices): improve series solution

--- practices/114-series/series.tcl
+++ practices/114-series/series.tcl
@@ -1,4 +1,23 @@
 proc slices {series length} {
-    throw {NOT_IMPLEMENTED} "Implement this procedure."
-}
+    set str_len [string length $series]
 
+    if {$series eq ""} {
+        error "series cannot be empty"
+    }
+    if {$length > $str_len} {
+        error "slice length cannot be greater than series length"
+    }
+    if {$length == 0} {
+        error "slice length cannot be zero"
+    }
+    if {$length < 0} {
+        error "slice length cannot be negative"
+    }
+
+    set result {}
+    for {set i 0} {$i <= [expr {$str_len - $length}]} {incr i} {
+        lappend result [string range $series $i [expr {$i + $length - 1}]]
+    }
+
+    return $result
+}
