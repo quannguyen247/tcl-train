27c3de7c5bd76ef0168dce0bf39fa637ff063a1b
quannguyen247 <nguyendongquan247@gmail.com>
2026-08-08 14:07:29 +0700

    refactor(practices): improve secret-handshake solution

--- practices/113-secret-handshake/secret-handshake.tcl
+++ practices/113-secret-handshake/secret-handshake.tcl
@@ -1,4 +1,9 @@
 proc secretHandshake {code} {
-    throw {NOT_IMPLEMENTED} "Implement this procedure."
+    set res {}
+    if {$code & 1} { lappend res "wink" }
+    if {$code & 2} { lappend res "double blink" }
+    if {$code & 4} { lappend res "close your eyes" }
+    if {$code & 8} { lappend res "jump" }
+    if {$code & 16} { set res [lreverse $res] }
+    return $res
 }
-
