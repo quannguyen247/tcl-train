10d72c400f706a126552b03dd7daef365c1fa105
quannguyen247 <nguyendongquan247@gmail.com>
2026-08-08 16:22:48 +0700

    refactor(practices): improve sieve solution

--- practices/116-sieve/sieve.tcl
+++ practices/116-sieve/sieve.tcl
@@ -1,4 +1,14 @@
 proc primes {limit} {
-    throw {NOT_IMPLEMENTED} "Implement this procedure."
+    set res {}
+    if {$limit < 2} { return {} }
+    set is_prime [lrepeat [expr {$limit + 1}] 1]
+    for {set i 2} {$i <= $limit} {incr i} {
+        if {[lindex $is_prime $i]} {
+            lappend res $i
+            for {set j [expr {$i * $i}]} {$j <= $limit} {incr j $i} {
+                lset is_prime $j 0
+            }
+        }
+    }
+    return $res
 }
-
