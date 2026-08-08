2c6b7d3ede6e06708b4639042b3bd66fdd931f1d
quannguyen247 <nguyendongquan247@gmail.com>
2026-08-08 13:10:46 +0700

    refactor(practices): improve scrabble-score solution

--- practices/112-scrabble-score/scrabble-score.tcl
+++ practices/112-scrabble-score/scrabble-score.tcl
@@ -1,4 +1,20 @@
 proc scrabbleScore {word} {
-    throw {NOT_IMPLEMENTED} "Implement this procedure."
-}
+    set scrabble_dict [dict create \
+        A 1 E 1 I 1 O 1 U 1 L 1 N 1 R 1 S 1 T 1 \
+        D 2 G 2 \
+        B 3 C 3 M 3 P 3 \
+        F 4 H 4 V 4 W 4 Y 4 \
+        K 5 \
+        J 8 X 8 \
+        Q 10 Z 10 \
+    ]
 
+    set score 0
+    foreach char [split [string toupper $word] ""] {
+        if {[dict exists $scrabble_dict $char]} {
+            incr score [dict get $scrabble_dict $char]
+        }
+    }
+
+    return $score
+}
