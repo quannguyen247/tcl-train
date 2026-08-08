b4f321fa068d9de2009bba5b01e8e4bc46f52e3a
quannguyen247 <nguyendongquan247@gmail.com>
2026-08-08 09:33:32 +0700

    refactor(practices): improve saddle-points solution

--- practices/108-saddle-points/saddle-points.tcl
+++ practices/108-saddle-points/saddle-points.tcl
@@ -1,4 +1,30 @@
 proc saddlePoints {matrix} {
-    throw {NOT_IMPLEMENTED} "Implement this procedure."
-}
+    if {$matrix eq {} || [lindex $matrix 0] eq {}} {
+        return {}
+    }
 
+    set result {}
+    set rows [llength $matrix]
+    set columns [llength [lindex $matrix 0]]
+
+    for {set rowIndex 0} {$rowIndex < $rows} {incr rowIndex} {
+        set row [lindex $matrix $rowIndex]
+        set maxInRow [lindex [lsort -integer $row] end]
+        for {set column 0} {$column < $columns} {incr column} {
+            set value [lindex $row $column]
+            if {$value != $maxInRow} {
+                continue
+            }
+
+            set minInColumn {}
+            for {set r 0} {$r < $rows} {incr r} {
+                lappend minInColumn [lindex $matrix $r $column]
+            }
+            if {$value == [lindex [lsort -integer $minInColumn] 0]} {
+                lappend result [list [expr {$rowIndex + 1}] \
+                    [expr {$column + 1}]]
+            }
+        }
+    }
+    return $result
+}
