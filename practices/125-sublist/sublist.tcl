fc1ce03a74384b85f38dc332834c3e6c4df060b8
quannguyen247 <nguyendongquan247@gmail.com>
2026-08-10 17:30:12 +0700

    refactor(practices): improve sublist solution

--- practices/125-sublist/sublist.tcl
+++ practices/125-sublist/sublist.tcl
@@ -1,4 +1,26 @@
 proc sublist {list1 list2} {
-    throw {NOT_IMPLEMENTED} "Implement this procedure."
-}
+    set len1 [llength $list1]
+    set len2 [llength $list2]
 
+    if {$list1 eq $list2} {
+        return "equal"
+    }
+
+    if {$len1 < $len2} {
+        for {set i 0} {$i <= $len2 - $len1} {incr i} {
+            if {[lrange $list2 $i [expr {$i + $len1 - 1}]] eq $list1} {
+                return "sublist"
+            }
+        }
+    }
+
+    if {$len1 > $len2} {
+        for {set i 0} {$i <= $len1 - $len2} {incr i} {
+            if {[lrange $list1 $i [expr {$i + $len2 - 1}]] eq $list2} {
+                return "superlist"
+            }
+        }
+    }
+
+    return "unequal"
+}
