a28afea04e3fb60180ca521fd91ccfde2ff86147
quannguyen247 <nguyendongquan247@gmail.com>
2026-08-10 14:49:13 +0700

    refactor(practices): improve state-of-tic-tac-toe solution

--- practices/123-state-of-tic-tac-toe/state-of-tic-tac-toe.tcl
+++ practices/123-state-of-tic-tac-toe/state-of-tic-tac-toe.tcl
@@ -1,4 +1,32 @@
 proc gamestate {board} {
-    throw {NOT_IMPLEMENTED} "Implement this procedure."
-}
+    set cells [join $board ""]
+    set x [regexp -all X $cells]
+    set o [regexp -all O $cells]
 
+    if {$x > $o + 1} {
+        error "Wrong turn order: X went twice"
+    }
+    if {$o > $x} {
+        error "Wrong turn order: O started"
+    }
+
+    set lines [concat $board [list \
+        "[string index [lindex $board 0] 0][string index [lindex $board 1] 0][string index [lindex $board 2] 0]" \
+        "[string index [lindex $board 0] 1][string index [lindex $board 1] 1][string index [lindex $board 2] 1]" \
+        "[string index [lindex $board 0] 2][string index [lindex $board 1] 2][string index [lindex $board 2] 2]" \
+        "[string index [lindex $board 0] 0][string index [lindex $board 1] 1][string index [lindex $board 2] 2]" \
+        "[string index [lindex $board 0] 2][string index [lindex $board 1] 1][string index [lindex $board 2] 0]"]]
+
+    set xWon [expr {"XXX" in $lines}]
+    set oWon [expr {"OOO" in $lines}]
+
+    if {$xWon && !$oWon && $x == $o + 1 || $oWon && !$xWon && $x == $o} {
+        return win
+    }
+
+    if {$xWon || $oWon} {
+        error "Impossible board: game should have ended after the game was won"
+    }
+
+    return [expr {[string first " " $cells] < 0 ? "draw" : "ongoing"}]
+}
