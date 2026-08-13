dec1226fc2f013bd8d8c63b333576643ee362022
quannguyen247 <nguyendongquan247@gmail.com>
2026-08-13 11:02:58 +0700

    refactor(practices): improve triangle solution

--- practices/130-triangle/triangle.tcl
+++ practices/130-triangle/triangle.tcl
@@ -1,6 +1,24 @@
 namespace eval triangle {
-    proc is {} {
-        throw {NOT_IMPLEMENTED} "Implement this procedure."
+    proc is {type sides} {
+        lassign $sides a b c
+
+        if {$a <= 0 || $b <= 0 || $c <= 0 || $a + $b < $c || $b + $c < $a || $a + $c < $b} {
+            return false
+        }
+
+        switch -- $type {
+            equilateral {
+                return [expr {$a == $b && $b == $c}]
+            }
+            isosceles {
+                return [expr {$a == $b || $b == $c || $a == $c}]
+            }
+            scalene {
+                return [expr {$a != $b && $b != $c && $a != $c}]
+            }
+            default {
+                return false
+            }
+        }
     }
 }
-
