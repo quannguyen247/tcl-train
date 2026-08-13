580a0d5dbc8a644a9dc858cd187c27d975a8b053
quannguyen247 <nguyendongquan247@gmail.com>
2026-08-13 12:27:22 +0700

    refactor(practices): improve twelve-days solution

--- practices/131-twelve-days/twelve-days.tcl
+++ practices/131-twelve-days/twelve-days.tcl
@@ -1,8 +1,46 @@
 proc verse {n} {
-    throw {NOT_IMPLEMENTED} "Implement this procedure."
+    set days {
+        first second third fourth fifth sixth
+        seventh eighth ninth tenth eleventh twelfth
+    }
+
+    set gifts {
+        {a Partridge in a Pear Tree.}
+        {two Turtle Doves}
+        {three French Hens}
+        {four Calling Birds}
+        {five Gold Rings}
+        {six Geese-a-Laying}
+        {seven Swans-a-Swimming}
+        {eight Maids-a-Milking}
+        {nine Ladies Dancing}
+        {ten Lords-a-Leaping}
+        {eleven Pipers Piping}
+        {twelve Drummers Drumming}
+    }
+
+    set lines {}
+    for {set i [expr {$n - 1}]} {$i >= 0} {incr i -1} {
+        set line [lindex $gifts $i]
+
+        if {$i > 0} {
+            append line ,
+        } elseif {$n > 1} {
+            set line "and $line"
+        }
+
+        lappend lines $line
+    }
+
+    return "On the [lindex $days [expr {$n - 1}]] day of Christmas my true love gave to me: [join $lines " "]"
 }
 
 proc sing {from to} {
-    throw {NOT_IMPLEMENTED} "Implement this procedure."
-}
+    set song {}
 
+    for {set day $from} {$day <= $to} {incr day} {
+        lappend song [verse $day]
+    }
+
+    return [join $song \n]
+}
