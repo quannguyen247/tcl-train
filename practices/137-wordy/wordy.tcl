1a0665e6faad65a43ab1f1c14b660d87406c1c7c
quannguyen247 <nguyendongquan247@gmail.com>
2026-08-16 12:26:47 +0700

    refactor(practices): improve wordy solution

--- practices/137-wordy/wordy.tcl
+++ practices/137-wordy/wordy.tcl
@@ -1,4 +1,52 @@
 proc answer {question} {
-    throw {NOT_IMPLEMENTED} "Implement this procedure."
-}
+    if {![regexp {^What is} $question] || ![regexp {\?$} $question]} {
+        error "unknown operation"
+    }
 
+    regexp {^What is\s*(.*?)\?$} $question -> text
+    set text [string trim $text]
+
+    if {$text eq ""} {
+        error "syntax error"
+    }
+
+    set text [string map {"multiplied by" "multiplied_by" "divided by" "divided_by"} $text]
+    set tokens [split $text " "]
+
+    set first [lindex $tokens 0]
+    if {![regexp {^-?\d+$} $first]} {
+        error "syntax error"
+    }
+
+    set result $first
+    set len [llength $tokens]
+    set idx 1
+
+    while {$idx < $len} {
+        set op [lindex $tokens $idx]
+
+        if {$op in {plus minus multiplied_by divided_by}} {
+            if {$idx + 1 >= $len} {
+                error "syntax error"
+            }
+            set next [lindex $tokens $idx+1]
+            if {![regexp {^-?\d+$} $next]} {
+                error "syntax error"
+            }
+
+            switch -- $op {
+                "plus"          { set result [expr {$result + $next}] }
+                "minus"         { set result [expr {$result - $next}] }
+                "multiplied_by" { set result [expr {$result * $next}] }
+                "divided_by"    { set result [expr {$result / $next}] }
+            }
+            incr idx 2
+        } elseif {[regexp {^-?\d+$} $op]} {
+            error "syntax error"
+        } else {
+            error "unknown operation"
+        }
+    }
+
+    return $result
+}
