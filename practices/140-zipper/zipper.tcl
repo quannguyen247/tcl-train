b812ac32aaf46ad37e919853e26bd5ffbdf0252b
quannguyen247 <nguyendongquan247@gmail.com>
2026-08-16 14:06:14 +0700

    refactor(practices): improve zipper solution

--- practices/140-zipper/zipper.tcl
+++ practices/140-zipper/zipper.tcl
@@ -1,8 +1,71 @@
 source "tree.tcl"
 
 oo::class create Zipper {
-    constructor {aTree} {
-        throw {NOT_IMPLEMENTED} "Implement this class."
+    variable focus parent side
+
+    constructor {aTree {aParent ""} {aSide ""}} {
+        set focus $aTree
+        set parent $aParent
+        set side $aSide
+    }
+
+    method tree {} {
+        if {$parent eq ""} {
+            return $focus
+        }
+        return [$parent tree]
+    }
+
+    method value {} {
+        return [$focus value]
+    }
+
+    method left {} {
+        set child [$focus left]
+        if {$child eq ""} {
+            return ""
+        }
+        return [Zipper new $child [self] left]
+    }
+
+    method right {} {
+        set child [$focus right]
+        if {$child eq ""} {
+            return ""
+        }
+        return [Zipper new $child [self] right]
+    }
+
+    method up {} {
+        return $parent
+    }
+
+    method setValue {value} {
+        $focus setValue $value
+        return [self]
+    }
+
+    method setLeft {tree} {
+        $focus setLeft $tree
+        return [self]
+    }
+
+    method setRight {tree} {
+        $focus setRight $tree
+        return [self]
+    }
+
+    method path {} {
+        if {$parent eq ""} {
+            return {}
+        }
+        return [concat [$parent path] $side]
+    }
+
+    method equals {other} {
+        return [expr {
+            [my path] eq [$other path]
+            && [[my tree] toDict] eq [[$other tree] toDict]
+        }]
     }
 }
-
